869db4ad028d36288cb465e3e5e1d0cb9636a6a8
Gabriele Zigurella <g.zigurella@nextmind.it>
2023-06-23 23:03:29 +0200

    add gitignore, fix return of loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import '../styles/loading.css'
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return  
+    return  (
         <div className='flex items-center justify-center min-h-screen min-w-screen h-screen w-screen'>
             <div className="lds-grid">
                 <div id="r1c1"></div>
@@ -16,4 +16,5 @@ export default function Loading() {
                 <div id="r3c3"></div>
             </div>
         </div>
+    );
   }
