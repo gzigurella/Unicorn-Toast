eb7c3d94a6fdd6f340a22e914570d9a0adc25927
Gabriele Zigurella <56323265+gzigurella@users.noreply.github.com>
2024-08-03 12:14:53 +0200

    Update version of postcss, added tooltip to Skills

--- src/components/ProjectModalBody.jsx
+++ src/components/ProjectModalBody.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import {Tooltip} from "react-tooltip";
 
 const fetchSkill = (skillName) => {
     return (
         <div className={'flex flex-col gap-y-0.5 justify-between items-center'}>
-            <Image key={skillName} src={`/Skills/${skillName}.svg`} alt={skillName} height={40} width={40} />
-            {/*TODO: add tooltip with skillName inside!*/}
+            <Tooltip id={skillName} place={"bottom"}/>
+            <Image data-tooltip-id={skillName} data-tooltip-content={skillName} key={skillName} src={`/Skills/${skillName}.svg`} alt={skillName} height={40} width={40} />
         </div>
     );
 }
@@ -19,7 +20,7 @@ const modalBody = ({description, skillNames}) => {
             {skillNames &&
                 <div className={'mt-4'}>
                     <div className={'text-sm font-mono text-gray-400 mb-2'}>
-                        Skills involved
+                        Technology Stack involved <span className={'hidden md:block text-xs'}>(hover to show tooltip)</span>
                     </div>
                     <div className={'flex flex-wrap flex-auto justify-start gap-x-4'}>
                         {skillNames.map((skillName) => fetchSkill(skillName))}
